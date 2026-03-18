c989bba21ae2a57e4df90b09da29d7be5a7fdfd6
Liya Tesfaye <liyagebrutesfaye@gmail.com>
2026-03-18 15:37:14 -0500

    Update: Added Timeout Handling

--- src/portscanner.c
+++ src/portscanner.c
@@ -5,6 +5,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 void port_scanner(char* ipaddress, char* low, char* high)
 {
@@ -13,19 +14,42 @@ void port_scanner(char* ipaddress, char* low, char* high)
 
         struct sockaddr_in sockadd;
 
-        for(int i = start; i < end; i++)
+        for(int i = start; i < end + 1; i++)
         {
                 sockadd.sin_family = AF_INET;
                 sockadd.sin_port = htons(i);
                 sockadd.sin_addr.s_addr = inet_addr(ipaddress);
 
                 int sock = socket(AF_INET, SOCK_STREAM, 0);
+		fcntl(sock, F_SETFL, O_NONBLOCK);
+
+		fd_set myset;
+		FD_ZERO(&myset);
+		FD_SET(sock, &myset);
+
                 int result = connect(sock, (struct sockaddr*)&sockadd, sizeof(sockadd));
 
-                if(result == 0)
-                        printf("Port %d: open\n", i);
-//                if(result == -1)
-//                        printf("Port %d: Closed\n", i);
+		struct timeval timeout;
+		timeout.tv_sec = 1;
+		timeout.tv_usec = 0;
+
+		int s = select(sock + 1, NULL, &myset, NULL, &timeout);
+		int err = -1;
+		socklen_t len = sizeof(err);
+
+		if(s == 0)
+		{
+			printf("Port %d Filtered\n", i);
+			close(sock);
+			continue;
+		}
+		else if(s == 1)
+			getsockopt(sock, SOL_SOCKET, SO_ERROR, &err, &len);
+
+		if(err == 0)
+                	printf("Port %d: open\n", i);
+		else
+			printf("Port %d: closed\n", i);
 
                 close(sock);
         }
